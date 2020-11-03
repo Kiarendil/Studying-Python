bee5f39f9d370575fe779a13983cb8165d24714f
Kiarendil Air <ivanov.km@phystech.edu>
2020-11-03 15:43:01 +0300

    test2

--- test.py
+++ test.py
@@ -2,8 +2,7 @@ from IPython.display import clear_output
 
 # Defining Gaussian PDF using tf functions
 def gaussian(x, mu, sigma):
-  return 1 / (2 * np.pi * sigma**2)**0.5 * tf.exp(-(x - mu)**2 / (2 * sigma**2))
-  # use tf.exp for the exponent, don't forget the normalization
+  return tf.exp(-(x - mu)**2 / (2*sigma**2)) / ((2*np.pi)**0.5 * sigma) # use tf.exp for the exponent, don't forget the normalization
 
 # Arbitrary mixture PDF
 def mixture(x, f1, f2, alpha):
@@ -19,10 +18,15 @@ alpha = tf.Variable(0.5)
 # At each gradient update we'll apply this function to
 # ensure valid parameter values
 def apply_constraints():
-  s1.assign(tf.maximum(s1, 1e-05)) # ensure the sigmas are positive (greater or equal to some small
-  s2.assign(tf.maximum(s2, 1e-05))            # positive number, e.g. 1e-5) and alpha is in (0, 1) interval
-  alpha.assign(tf.maximum(0., tf.minimum(1., alpha)))            # hint: use the `assign` method to update the variable values
-
+  s1.assign(tf.maximum(s1, 1e-6))
+  s2.assign(tf.maximum(s2, 1e-6))
+  if alpha < 0:
+    alpha.assign(0.)
+  elif alpha > 1:
+    alpha.assign(1.)
+              # ensure the sigmas are positive (greater or equal to some small
+              # positive number, e.g. 1e-5) and alpha is in (0, 1) interval
+              # hint: use the assign method to update the variable values
 
 variables = [m1, m2, s1, s2, alpha]
 
@@ -42,9 +46,9 @@ grad_history = [] # to keep track of gradient values at different steps
 
 for i in range(30):
   with tf.GradientTape() as t:
-    # NOTE: if `variable` was created with `trainable=False`, you need to
-    # manually add it to the tape by calling `t.watch(variable)`. By default
-    # the variable are created with `trainable=True`
+    # NOTE: if variable was created with trainable=False, you need to
+    # manually add it to the tape by calling t.watch(variable). By default
+    # the variable are created with trainable=True
 
     # Forward pass:
     l = loglike(X)
@@ -53,10 +57,12 @@ for i in range(30):
   grads = t.gradient(l, variables) # Calculate the gradients
   grad_history.append([g.numpy() for g in grads])
 
+  print(grads)
+
   # Gradient ascent step:
   for v, g in zip(variables, grads):
-    v.assign_add(learning_rate * g) # Apply the gradients manually (add
-              # `learning_rate * corresponding_gradient` to
+    v.assign(learning_rate * g)# Apply the gradients manually (add
+              # learning_rate * corresponding_gradient to
               # each variable)
   apply_constraints()
 
@@ -81,4 +87,4 @@ for i in range(30):
   plt.xlabel('step')
   plt.ylabel('gradient')
   plt.show()
-  clear_output(wait=True);
+  clear_output(wait=True); 
